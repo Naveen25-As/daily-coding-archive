77c3e5d3c02ec22d26dea68dac8e102d729e3eb2
Naveen asundi <naveenasundi03@gmail.com>
2026-03-17 19:18:20 +0530

    Create day-02allpro.py

---
+++ day-02allpro.py
@@ -0,0 +1,13 @@
+#Airthmetic operators
+num1=int(input("Enter first number:"))
+num2=int(input("Enter second number:"))
+sum=num1+num2
+sub=num1-num2
+mul=num1**num2
+div=num1/num2
+mod=num1%num2
+print(sum)
+print(sub)
+print(mul)
+print(div)
+print(mod)
