33938d755aabb3d47c30de0836b6c60dff6d19aa
Naveen asundi <naveenasundi03@gmail.com>
2026-03-19 10:20:33 +0530

    day-04 programs

---
+++ day-04program.py
@@ -0,0 +1,8 @@
+#swaping two numbers
+a=10
+b=9
+print("Before swaping",a,b,)
+temp=a
+a=b
+b=a
+print("After swaping",a,b)
---
+++ day-04program2.py
@@ -0,0 +1,6 @@
+#swaping without third variable
+a=10
+b=8
+print("Before swaping",a,b)
+a=b
+print("After swaping",a,b)
