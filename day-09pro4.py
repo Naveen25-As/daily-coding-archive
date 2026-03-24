caeabe70446984067014d540b7841d061c76b0ca
Naveen asundi <naveenasundi03@gmail.com>
2026-03-24 19:46:17 +0530

    day-09pro3.py

---
+++ day-09pro4.py
@@ -0,0 +1,13 @@
+# print both even and odd
+
+n = int(input("Enter a number : "))
+
+print("Even numbers : ")
+for i in range(1, n + 1):
+    if i % 2 == 0:
+        print(i)
+
+print("Odd numbers : ")
+for i in range(1, n + 1):
+    if i % 2 != 0:
+        print(i)
