f65c16017144a675bc00aae9772d516be42b6750
Naveen asundi <naveenasundi03@gmail.com>
2026-03-25 21:22:43 +0530

    Organized files into day-wise folders

---
+++ Day10/day-10pro3.py
@@ -0,0 +1,9 @@
+# Find the smallest of two number
+
+a = int(input("Enter first number : "))
+b = int(input("Enter second number : "))
+
+if a<b:
+    print("Smaller number is : ",a)
+else:
+    print("Smallest number is : ",b)
