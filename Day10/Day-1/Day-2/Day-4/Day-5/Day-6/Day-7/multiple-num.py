7f1a21c32d31691fe78cdd80818a5719782e9f9d
Naveen asundi <naveenasundi03@gmail.com>
2026-03-27 20:12:07 +0530

    Add files via upload

---
+++ Day10/Day-1/Day-2/Day-4/Day-5/Day-6/Day-7/multiple-num.py
@@ -0,0 +1,7 @@
+# Print multiple of a given number
+
+num = int(input("Enter a number : "))
+limit = int(input("Enter limit : "))
+
+for i in range(1, limit + 1):
+    print(num + i)
---
+++ Day10/Day-1/Day-2/Day-4/Day-5/Day-6/Day-7/n-to-1.py
@@ -0,0 +1,6 @@
+# Print numbers n to 1
+
+n = int(input("Enter a number : "))
+
+for i in range(n, 0 , -1):
+    print(i)
