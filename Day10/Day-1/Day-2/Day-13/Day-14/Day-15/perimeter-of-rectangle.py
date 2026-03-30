818b8fd642d87f02229a3ee63162791801f03cd0
Naveen asundi <naveenasundi03@gmail.com>
2026-03-30 20:15:19 +0530

    perimeter-of-rectangle.py

---
+++ Day10/Day-1/Day-2/Day-13/Day-14/Day-15/perimeter-of-rectangle.py
@@ -0,0 +1,7 @@
+# Calculate parameter of rectangle
+
+length = float(input("Enter length of a rectangle : "))
+width = float(input("Enter width of a rectangle : "))
+
+perimeter = 2 * (length +  width)
+print("Perimeter of rectangle : ",perimeter)
