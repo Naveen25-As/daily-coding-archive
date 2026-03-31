4ed2856f1537ba05de0a173968b6cbc07021addb
Naveen asundi <naveenasundi03@gmail.com>
2026-03-31 18:52:40 +0530

    Circumference-of-Circle.py

---
+++ Day10/Day-1/Day-2/Day-13/Day-14/Day-15/Circumference-of-Circle.py
@@ -0,0 +1,6 @@
+# Calculate Circumference of circle
+import math
+r = float(input("Enter radius os a circle : "))
+circumference = 2 * math.pi * r
+
+print("Circumference of circle : ",circumference)
