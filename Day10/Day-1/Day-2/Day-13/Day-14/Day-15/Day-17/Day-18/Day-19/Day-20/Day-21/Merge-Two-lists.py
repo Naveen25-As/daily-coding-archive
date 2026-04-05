939d6f5dfe6cac8992f7b7a9ee9615b7baf4f04c
Naveen asundi <naveenasundi03@gmail.com>
2026-04-05 15:20:13 +0530

    Merge-Two-lists.py

---
+++ Day10/Day-1/Day-2/Day-13/Day-14/Day-15/Day-17/Day-18/Day-19/Day-20/Day-21/Merge-Two-lists.py
@@ -0,0 +1,14 @@
+# Merge Two lists
+
+list1 = [1,2,3]
+list2 = [4,5,6]
+
+merged_list = []
+
+for i in list1:
+    merged_list.append(i)
+
+for j in list2:
+    merged_list.append(j)
+    
+print("Merged list : ",merged_list)
