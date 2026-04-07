d7f0b28b4988feba3bfb0dee20f46c3883259937
Naveen asundi <naveenasundi03@gmail.com>
2026-04-07 17:56:21 +0530

    Concatinate-of-two-strings.py

---
+++ Day10/Day-1/Day-2/Day-13/Day-14/Day-15/Day-17/Day-18/Day-19/Day-20/Day-21/Day-22/Day-23/concantinate-of-two-strings.py
@@ -0,0 +1,9 @@
+# Write a program to create, concatenate and print a string and accessing sub- string from a given string.
+
+string1 = input("Enter first string  : ")
+string2 = input("Enter second string : ")
+
+print("First string is : ",string1)
+print("Second string is : ",string2)
+print("Concantinate of two string ",string1+string2)
+print("Substring of given string : ",string1[1::2])
