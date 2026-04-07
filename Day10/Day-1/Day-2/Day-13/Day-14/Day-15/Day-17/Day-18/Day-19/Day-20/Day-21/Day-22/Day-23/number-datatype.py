c077f8e7c0b17ecdddd478d291caeebd084c21c2
Naveen asundi <naveenasundi03@gmail.com>
2026-04-07 15:36:33 +0530

    Number-datatype.py

---
+++ Day10/Day-1/Day-2/Day-13/Day-14/Day-15/Day-17/Day-18/Day-19/Day-20/Day-21/Day-22/Day-23/number-datatype.py
@@ -0,0 +1,9 @@
+# Write a program to demonstrate different number datatypes in python.
+
+num1 = 2
+num2  = 2.2
+num3 = 2+3j
+
+print("num1 is type of : ",type(num1))
+print("num2 is type of : ",type(num2))
+print("num2 is type of : ",type(num3))
