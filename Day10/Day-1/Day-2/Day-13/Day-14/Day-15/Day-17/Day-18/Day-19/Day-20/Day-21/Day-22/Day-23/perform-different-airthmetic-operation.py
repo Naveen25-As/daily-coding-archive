81fc770ec0ea916520a660f2bfe07bfcfc35c363
Naveen asundi <naveenasundi03@gmail.com>
2026-04-07 15:37:22 +0530

    Perform-different-Arithmetic-operation.py

---
+++ Day10/Day-1/Day-2/Day-13/Day-14/Day-15/Day-17/Day-18/Day-19/Day-20/Day-21/Day-22/Day-23/perform-different-airthmetic-operation.py
@@ -0,0 +1,16 @@
+# Write a program to perform different Arithmetic Operations on numbers in python.
+
+num1 = int(input("Enter first number : "))
+num2 = int(input("Enter second number : "))
+
+sum = num1 + num2
+sub = num1 - num2
+mul = num1 * num2
+div = num1 / num2
+mod = num1 % num2
+
+print("Auddition : ",sum)
+print("Subraction : ",sub)
+print("Multiplication : ",mul)
+print("Division : ",div)
+print("Mod : ",mod)
