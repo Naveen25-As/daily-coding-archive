69daea44bc18b7229ec522eee69783848fd307ed
Naveen asundi <naveenasundi03@gmail.com>
2026-04-07 17:57:05 +0530

    Fabonacci-series.py

---
+++ Day10/Day-1/Day-2/Day-13/Day-14/Day-15/Day-17/Day-18/Day-19/Day-20/Day-21/Day-22/Day-23/Fabonacci-series.py
@@ -0,0 +1,13 @@
+# Print fabonacci series
+
+num = 5
+n1 = 0
+n2 = 1
+
+print("Fabonacci series : ",n1,n2, end=" " )
+for i in range(2,num):
+    n3 = n1 + n2
+    n1 = n2
+    n2 = n3
+    print(n3,end =" ")
+print()
