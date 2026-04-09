54114e9557894316bffdc8914ad555b838ac0a63
Naveen asundi <naveenasundi03@gmail.com>
2026-04-09 16:26:14 +0530

    Functions-of-tuple.py

---
+++ Day10/Day-1/Day-2/Day-13/Day-14/Day-15/Day-17/Day-18/Day-19/Day-20/Day-21/Day-22/Day-23/Day-24/Day-25/Fuctions-of-tuple.py
@@ -0,0 +1,10 @@
+# Functions of tuple
+
+tuple = (1,2,3,4,5)
+print("Index of 3 :",tuple.index(3))
+print("Count of 3 : ",tuple.count(3))
+print("Length of tuple : ", len(tuple))
+print("Sum of tuple : ", sum(tuple))
+print("Minimum of tuple : ", min(tuple))
+print("Maximun of tuple : ", max(tuple))
+
