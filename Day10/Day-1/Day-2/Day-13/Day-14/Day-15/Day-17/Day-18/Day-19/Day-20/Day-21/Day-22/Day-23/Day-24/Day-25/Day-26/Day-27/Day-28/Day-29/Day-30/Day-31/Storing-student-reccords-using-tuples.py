d80bfed0e30c8b7e7bc4ba08800b5d3976011d32
Naveen asundi <naveenasundi03@gmail.com>
2026-04-16 16:52:46 +0530

    Storing-students-records-using-tuples.py

---
+++ Day10/Day-1/Day-2/Day-13/Day-14/Day-15/Day-17/Day-18/Day-19/Day-20/Day-21/Day-22/Day-23/Day-24/Day-25/Day-26/Day-27/Day-28/Day-29/Day-30/Day-31/Storing-student-reccords-using-tuples.py
@@ -0,0 +1,15 @@
+# Storing student reccords using tuples
+
+student=[
+    (101,"Manu",82),
+    (102,"Guru",72),
+    (103,"Nikil",70),
+    (104,"Naveen",84),
+    (105,"Ritvik",94)
+]
+
+# Diplay all students records
+
+print("Student records : ")
+for s in student:
+    print(s[0],s[1],s[2])
