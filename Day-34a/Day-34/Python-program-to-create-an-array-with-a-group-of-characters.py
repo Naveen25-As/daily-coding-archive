850c4872f0830a9de2ab28135a9797e5ab4d4689
Naveen asundi <naveenasundi03@gmail.com>
2026-04-19 16:59:01 +0530

    copy-of-programs.py

---
+++ Day-34a/Day-34/Python-program-to-create-an-array-with-a-group-of-characters.py
@@ -0,0 +1,11 @@
+# A Python program to create an array with a group of characters.
+
+from array import *
+
+arr = array('u',['a','b','c','d','e'])
+
+print("Array elements are : ")
+
+for ch in arr:
+    print(ch)
+
