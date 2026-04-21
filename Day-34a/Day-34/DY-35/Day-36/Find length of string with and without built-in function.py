98aec2952b143deda283ac6afcadca59f31797de
Naveen asundi <naveenasundi03@gmail.com>
2026-04-21 15:24:01 +0530

    Find length of string with and without built-in function.py

---
+++ Day-34a/Day-34/DY-35/Day-36/Find length of string with and without built-in function.py
@@ -0,0 +1,14 @@
+# Find length of string with and without built-in function.
+
+#with functon
+
+s = "Python"
+print(len(s))
+
+# Without function
+
+s2 = "Java"
+count = 0
+for i in s2:
+    count += 1
+print(count)
