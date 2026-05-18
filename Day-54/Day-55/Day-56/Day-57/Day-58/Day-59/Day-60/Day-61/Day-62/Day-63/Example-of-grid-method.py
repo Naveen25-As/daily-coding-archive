dea3c565b8ff716667d4841e9e4626b79b14677f
Naveen asundi <naveenasundi03@gmail.com>
2026-05-18 15:52:34 +0530

    Example-of-grid-method.py

---
+++ Day-54/Day-55/Day-56/Day-57/Day-58/Day-59/Day-60/Day-61/Day-62/Day-63/Example-of-grid-method.py
@@ -0,0 +1,12 @@
+# Example of a Grid method.
+
+import tkinter as tk
+
+root = tk.Tk()
+
+button1 = tk.Button(root,text="Button 1")
+button1.grid(row = 0,column = 0)
+
+button2 = tk.Button(root,text="Button 2")
+button2.grid(row = 0,column = 1)
+root.mainloop()
