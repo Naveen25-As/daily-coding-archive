fce82e8d22b735a60f8cafeb0950f3ac2c024516
Naveen asundi <naveenasundi03@gmail.com>
2026-06-08 20:51:40 +0530

    Write-a-python-program-for-Calculator-class.py

---
+++ Day-84/Write-a-python-program-for-Calculator-class.py
@@ -0,0 +1,13 @@
+# Write a python program for Calculator class.
+
+class Calculator:
+    def add(Self,a,b):
+        return a + b
+
+    def sub(self,a,b):
+        return a - b
+
+c = Calculator()
+
+print("Addition=",c.add(10,5))
+print("Subtraction=",c.sub(10,5))
