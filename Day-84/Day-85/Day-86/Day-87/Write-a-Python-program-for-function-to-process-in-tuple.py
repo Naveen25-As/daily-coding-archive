1e240ee5c57d10bdc0de572c1bfaa84a62f073f9
Naveen asundi <naveenasundi03@gmail.com>
2026-06-11 21:18:44 +0530

    Write-a-Python-program-for-function-to-process-in-tuple.py

---
+++ Day-84/Day-85/Day-86/Day-87/Write-a-Python-program-for-function-to-process-in-tuple.py
@@ -0,0 +1,12 @@
+# Write a Python program for function to process in tuple. 
+
+def process_tuple(t):
+    print("Tuple Elements:",t)
+    print("Length:",len(t))
+    print("Maximim:",max(t))
+    print("Minimum:",min(t))
+    print("Sum:",sum(t))
+
+t = (10,20,30,40,50)
+
+process_tuple(t)
