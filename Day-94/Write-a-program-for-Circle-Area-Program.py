8b38bfad9328e8c7c43e03aca48e762fb39520da
Naveen asundi <naveenasundi03@gmail.com>
2026-06-18 20:20:41 +0530

    Write-a-program-for-Circle-Area-Program.py

---
+++ Day-94/Write-a-program-for-Circle-Area-Program.py
@@ -0,0 +1,11 @@
+# Write a program for Circle Area Program.
+
+class Circle:
+    def __init__(self,radius):
+        self.radius = radius
+
+    def area(self):
+        return 3.14 * self.radius * self.radius
+
+c = Circle(7)
+print("Area of Circle :",c.area())
